3b7564419eae26af666e88d7abe33321366b1715
IrrationalCatDev <lorinsantucci@gmail.com>
2023-09-20 22:48:24 -0400

    minor update
    refactored inventory interface and moved chest.gd to the correct location in the inventory folder.

--- Inventory/InventoryInterface.gd
+++ Inventory/InventoryInterface.gd
@@ -47,33 +47,28 @@ func on_inventory_interact(inventory_data: InventoryData, index: int, event: Inp
 	match [grabbed_slot_data, event.button_index]:
 		[null, MOUSE_BUTTON_LEFT]:
 			grabbed_slot_data = inventory_data.grab_slot_data(index)
-			if Input.is_key_pressed(KEY_SHIFT):
-				var success : bool = false
-				if external_inventory_owner and external_inventory_owner.inventory_data == inventory_data:
-					success = player_inventory.inventory_data.pick_up_slot_data(grabbed_slot_data)
-				else:
-					success = external_inventory_owner.inventory_data.pick_up_slot_data(grabbed_slot_data)
-				if success:
-					grabbed_slot_data = null
+			grabbed_slot_data = throw_slot_data_to_other_inventory(grabbed_slot_data,inventory_data)
 		[null, MOUSE_BUTTON_RIGHT]:
 			grabbed_slot_data = inventory_data.split_slot_data(index)
-			if grabbed_slot_data:
-				if Input.is_key_pressed(KEY_SHIFT):
-					var success : bool = false
-					if external_inventory_owner and external_inventory_owner.inventory_data == inventory_data:
-						success = player_inventory.inventory_data.pick_up_slot_data(grabbed_slot_data)
-					else:
-						success = external_inventory_owner.inventory_data.pick_up_slot_data(grabbed_slot_data)
-					if success:
-						grabbed_slot_data = null
+			grabbed_slot_data = throw_slot_data_to_other_inventory(grabbed_slot_data,inventory_data)
 		[_, MOUSE_BUTTON_LEFT]:
 			grabbed_slot_data = inventory_data.drop_slot_data(grabbed_slot_data,index)
 		[_, MOUSE_BUTTON_RIGHT]:
 			grabbed_slot_data = inventory_data.drop_split_slot_data(grabbed_slot_data,index)
 	
-	
-	
 	update_grabbed_slot()
+	
+func throw_slot_data_to_other_inventory(slot_data, inventory_data):
+	if slot_data:
+		if Input.is_key_pressed(KEY_SHIFT):
+			var success : bool = false
+			if external_inventory_owner and external_inventory_owner.inventory_data != inventory_data:
+				success = external_inventory_owner.inventory_data.pick_up_slot_data(slot_data)
+			else:
+				success = player_inventory.inventory_data.pick_up_slot_data(slot_data)
+			if success:
+				slot_data = null
+	return slot_data
 
 func update_grabbed_slot() -> void:
 	if grabbed_slot_data:
